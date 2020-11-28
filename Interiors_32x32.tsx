4c5f35b8a56f4d4f62cdb02f5fdef0f1b70812e3
maximehuran <m.huran@monsieurbiz.com>
2020-11-28 18:26:06 +0000

    Deploying to gh-pages from master @ 14cb8e7cbff5a35371a24194c930688bbc76209d 🚀

--- Interiors_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="Interiors_32x32" tilewidth="32" tileheight="32" tilecount="5360" columns="16">
- <image source="../../../Downloads/Modern_Interiors/1_Tilesets/32x32/Interiors_32x32.png" width="512" height="10720"/>
-</tileset>
